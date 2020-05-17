132132a4deef4e0d7e36d6ae2f7b9b80bc6d04d3
AngrySegin <lukasz-segin@o2.pl>
2020-05-17 14:57:55 +0200

    This is 3D game but it looks like 2D game. Destroy objects after mouse click. I can use it in my boar project.

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -5,19 +5,49 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     private Rigidbody targetRb;
+    private float minSpeed = 12;
+    private float maxSpeed = 16;
+    private float maxTorque = 10;
+    private float xRange = 4;
+    private float yRange = -2;
 
     // Start is called before the first frame update
     void Start()
     {
         targetRb = GetComponent<Rigidbody>();
-        targetRb.AddForce(Vector3.up * Random.Range(12, 16), ForceMode.Impulse);
-        targetRb.AddTorque(Random.Range(-10, 10), Random.Range(-10, 10), Random.Range(-10, 10), ForceMode.Impulse);
-        transform.position = new Vector3(Random.Range(-4,4), -6);
+        targetRb.AddForce(randomForce(), ForceMode.Impulse);
+        targetRb.AddTorque(randomTorque(), randomTorque(), randomTorque(), ForceMode.Impulse);
+        transform.position = randomSpawnPos();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+
+    Vector3 randomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+
+    float randomTorque()
+    {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+
+    Vector3 randomSpawnPos()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), yRange);
     }
 }
